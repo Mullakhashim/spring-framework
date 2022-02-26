bb82fc5b40a6a41bb213dcb9242305972acaca72
Mullakhashim <mullahashimsimai@gmail.com>
2022-02-26 17:15:40 -0500

    stereotype_annotation Practice

---
+++ Spring-02-Core-Wire/src/main/java/com/cydeo/Car.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Car {
+    private String make;
+
+
+
+}
---
+++ Spring-02-Core-Wire/src/main/java/com/cydeo/Person.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Person {
+    private String name;
+    private Car car;
+
+}
