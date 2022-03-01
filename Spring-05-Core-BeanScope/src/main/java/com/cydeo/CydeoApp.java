3fecce2a8e5fcddfa5f0d9889db51628ed5b0571
Mullakhashim <mullahashimsimai@gmail.com>
2022-02-28 22:01:40 -0500

    @Scope, @Lazy, prototype

---
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
@@ -0,0 +1,35 @@
+package com.cydeo;
+
+import com.cydeo.config.ProjectConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+        Comment comment =new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring-Framework");
+
+        ApplicationContext context=new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+       //  CommentService commentService=context.getBean(CommentService.class);
+       // commentService.publishComment(comment);
+
+        System.out.println("*******************************");
+//
+//        CommentService cs1=context.getBean(CommentService.class);
+//        CommentService cs2=context.getBean(CommentService.class);
+//
+//        System.out.println("cs1 = " + cs1);
+//        System.out.println("cs2 = " + cs2);
+//        System.out.println(cs1==cs2);
+
+
+
+
+
+    }
+}
